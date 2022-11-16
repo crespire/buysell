bbd5cd769bf753056ae2c3d9851f87ab164d59ae
crespire <li.simmon@gmail.com>
2022-11-16 11:40:09 -0500

    WIP: AuthProvider
    * Update client to use button to fire signUp
    * Update client to capture API response and update provider state

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,16 @@
+import React, { useEffect } from 'react';
+import { useAuth } from './providers/AuthProvider';
 
 function App() {
+  const { signUp } = useAuth();
+  
+  const handleClick = () => {
+    signUp('test2@test.com', '123456');
+  };
+
   return (
     <div className="App">
-      App stuff
+      <button onClick={handleClick}>Fire signup</button>
     </div>
   );
 }
--- client/src/providers/AuthProvider.tsx
+++ client/src/providers/AuthProvider.tsx
@@ -1,3 +1,4 @@
+import { UserInfo } from 'os';
 import React, {
   ReactNode,
   useEffect,
@@ -9,7 +10,7 @@ import React, {
 export type User = {
   login: string,
   status: 'verified' | 'unverified' | 'closed',
-
+  name: string,
 }
 
 export interface AuthProviderProps {
@@ -18,4 +19,56 @@ export interface AuthProviderProps {
 
 export interface AuthContextModel {
   user: User | null;
+  signUp: (email: string, password: string) => Promise<void>;
+  logIn: (email: string, password: string) => Promise<void>;
+  logOut: () => Promise<void>;
+}
+
+export const AuthContext = createContext<AuthContextModel>(
+  {} as AuthContextModel
+)
+
+export function useAuth(): AuthContextModel {
+  return useContext(AuthContext);
+}
+
+export const AuthProvider = ({children}: AuthProviderProps): JSX.Element => {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    console.log('AuthContext user changed!');
+    console.log(user);
+  }, [user]);
+
+  async function signUp(email: string, password: string): Promise<void> {
+    const requestData = JSON.stringify({"login": email, "password": password});
+    await fetch('http://localhost:3000/create-account', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: requestData
+    }).then(response => response.json()).then(data => {
+      console.log('Got a response:', data);
+      setUser(data.user);
+      console.log('Set user in state');
+    }).catch(error => console.log(error))
+  }
+
+  async function logIn(email: string, password: string): Promise<void> {
+  }
+
+  async function logOut(): Promise<void> {
+    setUser(null);
+  }
+
+  const values = {
+    user,
+    signUp,
+    logIn,
+    logOut,
+    //resetPassword
+  }
+  return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>
 }
