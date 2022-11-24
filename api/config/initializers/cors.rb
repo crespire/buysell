5a13cedec60ea3eb1242e0344a0d4ff285ffa77c
crespire <li.simmon@gmail.com>
2022-11-24 13:21:25 -0500

    feat: add CORS HttpOnly credentials

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -7,9 +7,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001', '127.0.0.1:3001'
-    resource '*', headers: :any, methods: %i[get post put patch delete options head]
+    origins 'http://localhost:3001', '127.0.0.1:3001', 'http://localhost:3000', '127.0.0.1:3000'
+    resource '*', headers: :any, methods: %i[get post put patch delete options head], credentials: true
   end
 end
 
-Rails.application.config.hosts << 'localhost'
+Rails.application.config.hosts << 'localhost:3001'
+Rails.application.config.hosts << 'localhost:3000'
