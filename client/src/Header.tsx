fc9e28441eaf8533e6b2fed3a5d8555952198989
crespire <li.simmon@gmail.com>
2022-11-24 11:56:00 -0500

    feat: add auth'd user greeting

--- client/src/Header.tsx
+++ client/src/Header.tsx
@@ -1,12 +1,17 @@
 import { Link } from "react-router-dom";
+import { useAuth } from './providers/AuthProvider';
 
 function Header() {
+  const { user } = useAuth();
+
   return (
     <div>
       <nav>
         <Link to='/'>Home</Link>
         <Link to='/signup'>Sign Up</Link>
+        <Link to='/signin'>Sign In</Link>
       </nav>
+      { user && <span>Hello { user.name }!</span>}
     </div>
   );
 }
