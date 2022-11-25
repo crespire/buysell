2034841b31caa60f3f2fb7ef39d4963d7d9a1fa7
crespire <li.simmon@gmail.com>
2022-11-24 19:53:57 -0500

    feat: add one user and one post to DB seeder

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+user = Account.create(status: 'verified', name: 'test_user1', email: 'test1@test.com', password: 'pass1234')
+user.posts.build(title: 'Test Title', body: 'Test Body Content').save
