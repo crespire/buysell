63c70e1d429150a89a5a7998fdf4221507247347
crespire <li.simmon@gmail.com>
2022-12-01 16:13:54 -0500

    fix: remove unused imports

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,3 @@
-import React, { useEffect } from 'react';
-import { useAuth } from './providers/AuthProvider';
 import { Outlet } from 'react-router';
 import Header from './Header';
 import Footer from './Footer';
