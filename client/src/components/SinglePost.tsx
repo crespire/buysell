3d0e16dd0cc8e4ac754b6292010b6e97d33bbe30
crespire <li.simmon@gmail.com>
2022-12-09 00:10:51 -0500

    feat: fix error handling in query functions

--- client/src/components/SinglePost.tsx
+++ client/src/components/SinglePost.tsx
@@ -6,14 +6,12 @@ import { useQuery } from '@tanstack/react-query';
 function SinglePost(): JSX.Element {
   const { id } = useParams();
   const { isLoading, isError, data, error } = useQuery({
-    queryKey: ['posts'],
+    queryKey: ['post', id],
     queryFn: () => getPost(id)
   });
 
-  console.log(data);
-
   if (isLoading) return <p>Loading...</p>;
-  if (isError && error instanceof Error) return <p>Post not found: {error.message}</p>
+  if (isError && error instanceof Error) return <p>Error: {error.message}</p>
 
   return <Post key={data?.id} post={data} />;
 }
--- client/src/providers/PostsApi.ts
+++ client/src/providers/PostsApi.ts
@@ -1,32 +1,36 @@
 import { baseUrl } from "..";
 import { PostModel } from "../@types/post";
 
-export async function fetchPosts(): Promise<PostModel[] | Record<string, any>> {
-  return await fetch(`${baseUrl}/posts`, {
+export async function fetchPosts(): Promise<PostModel[]> {
+  const response = await fetch(`${baseUrl}/posts`, {
       mode: 'cors',
       headers: {
         'Content-Type': 'application/json'
       },
       credentials: 'include'
-    })
-  .then((response) => response.json())
-  .catch((err) => {
-    console.log(err);
-    return {"error": err};
-  });
+    });
+  const data = await response.json();
+  
+  if (!response.ok) {
+    throw new Error(`${response.status} ${data.exception.split(':').at(-1).trim().replace('>', '')}`);
+  }
+
+  return data;
 };
 
-export async function getPost(id?: string): Promise<PostModel | Record<string, any>> {
-  return await fetch(`${baseUrl}/posts/${id}`, {
+export async function getPost(id?: string): Promise<PostModel> {
+  const response = await fetch(`${baseUrl}/posts/${id}`, {
       mode: 'cors',
       headers: {
         'Content-Type': 'application/json'
       },
       credentials: 'include'
-    })
-  .then((response) => response.json())
-  .catch((err) => {
-    console.log(err);
-    return {"error": err};
-  });
-}
+    });
+  const data = await response.json();
+  
+  if (!response.ok) {
+    throw new Error(`${response.status} ${data.exception.split(':').at(-1).trim().replace('>', '')}`);
+  }
+
+  return data;
+};
