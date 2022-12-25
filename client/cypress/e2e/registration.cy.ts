7a3db1dc8bb67908734ff4dc4ea7d907255fd7ba
crespire <li.simmon@gmail.com>
2022-12-24 21:57:21 -0500

    wip: stub posts response?

--- client/cypress/e2e/registration.cy.ts
+++ client/cypress/e2e/registration.cy.ts
@@ -1,5 +1,14 @@
 describe('My First Test', () => {
   it('Visits the Home Page', () => {
     cy.visit('/')
+    /**
+     * cy.intercept('POST', '/posts', {
+     *  statusCode: 201,
+     *  body: {
+     *    posts: 'Peter Pan',
+     *  },
+     * });
+     */
+    
   });
-})
+});
