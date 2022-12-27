3d110fd151aedc06e0c145e07ade86e40c721aa3
crespire <li.simmon@gmail.com>
2022-12-27 18:27:15 -0500

    wip: add user registration e2e test

--- client/cypress/e2e/post_index.cy.ts
+++ client/cypress/e2e/post_index.cy.ts
@@ -1,3 +1,5 @@
+/// <reference types="cypress"/>
+
 describe('Post Index', () => {
   it('displays public posts', () => {
     cy.visit('/')
---
+++ client/cypress/e2e/register_account.cy.ts
@@ -0,0 +1,17 @@
+/// <reference types="cypress"/>
+
+describe('Register User', () => {
+  it('Correctly registers a user', () => {
+    cy.visit('/')
+    cy.contains('Sign Up').click();
+    cy.location('pathname').should('eq', '/signup');
+    cy.get('input[name=email]').type("testuser@test.com");
+    cy.get('input[name=pass]').type('pass1234');
+    cy.get('input[name=passconf').type('pass1234');
+    cy.get('input[name=name').type('testuser1')
+    cy.get('form').submit();
+    cy.intercept('POST', '/create-account', { fixture: 'user.json' }).as('userCreate');
+    cy.location('pathname').should('eq', '/');
+    cy.get('nav').contains('testuser1');
+  });
+});
