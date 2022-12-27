767ff1b5634eb2e4713e1961161f8b6836b0793e
crespire <li.simmon@gmail.com>
2022-12-27 00:15:16 -0500

    fix: move stub response data to fixture

---
+++ client/cypress/e2e/postindex.cy.ts
@@ -0,0 +1,9 @@
+describe('Post Index', () => {
+  it('displays public posts', () => {
+    cy.visit('/')
+    cy.intercept('GET', '/posts', { fixture: 'posts.json' });
+    cy.get('[data-testid="post-index"]').contains('Test Post 1 Body');
+    cy.get('[data-testid="post-index"]').contains('Test Post 2 Body');
+    cy.get('[data-testid="post-index"]').children().should('have.length', 2);
+  });
+});
