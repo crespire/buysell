a84d91ecf18d1583e7b0ce9c8416b1ad6536cf2b
crespire <li.simmon@gmail.com>
2023-01-15 17:24:30 -0500

    feat: finish edit account e2e tests

--- client/cypress/e2e/edit_account.cy.ts
+++ client/cypress/e2e/edit_account.cy.ts
@@ -23,6 +23,7 @@ describe('Edit User', () => {
     });
 
     it('does not allow users to set an invalid username', () => {
+      // Too short
       cy.get("input[name='name']").type('tu');
       cy.get("input[name='password']").type('somepassword12');
       cy.get('p.error').should('contain', 'Must be between 3 and 36 characters, and only contain word characters.');
@@ -30,6 +31,7 @@ describe('Edit User', () => {
       cy.get('input:invalid').should('have.length', 1)
       cy.get("input[name='name']").clear();
       cy.get('p.error').should('not.exist');
+      // Invalid character
       cy.get("input[name='name']").type('invalid!');
       cy.get("input[name='password']").clear().type('somepassword12');
       cy.get('p.error').should('contain', 'Must be between 3 and 36 characters, and only contain word characters.');
@@ -37,6 +39,7 @@ describe('Edit User', () => {
       cy.get('input:invalid').should('have.length', 1)
       cy.get("input[name='name']").clear();
       cy.get('p.error').should('not.exist');
+      // Too long
       cy.get("input[name='name'").type('averylongusernamethatshouldbetoolongyes');
       cy.get("input[name='password']").clear().type('somepassword12');
       cy.get('p.error').should('contain', 'Must be between 3 and 36 characters, and only contain word characters.');
@@ -44,17 +47,32 @@ describe('Edit User', () => {
       cy.get('input:invalid').should('have.length', 1)
     });
 
-    it.skip('allows a user to change their password', () => {
+    it('allows a user to change their password', () => {
       cy.get("input[name='password']").type('pass1234');
       cy.get("input[name='new-password']").type('password8');
       cy.get("input[name='passconf']").type('password8');
-      cy.intercept('PATCH', '/account', { fixture: 'password_change_success.json' }).as('userPasswordSuccess');
-      cy.get('form').submit();
+      cy.intercept('POST', '/change-password', { fixture: 'password_change_success.json' }).as('userPasswordSuccess');
+      cy.get("button[type='submit'").click();
       cy.get('.authinfo').should('contain', 'Changes were successful!');
     });
 
-    it.skip('does not allow users to set a non-allowed password', () => {
-      // test with an invalid password
+    it('does not allow users to set a non-allowed password', () => {
+      // Invalid password
+      cy.get("input[name='password']").type('pass1234');
+      cy.get("input[name='new-password']").type('password');
+      cy.get("input[name='passconf']").type('password');
+      cy.get('p.error').should('contain', 'Minimum length is 8, and must contain a digit.');
+      cy.get("button[type='submit'").click();
+      cy.get('input:invalid').should('have.length', 1)
+      // Mismatched passwords
+      cy.get("input[name='new-password']").clear();
+      cy.get("input[name='passconf']").clear();
+      cy.get('p.error').should('not.exist');
+      cy.get("input[name='new-password']").type('password4');
+      cy.get("input[name='passconf']").type('password2');
+      cy.get('p.error').should('contain', 'Passwords must match.');
+      cy.get("button[type='submit'").click();
+      cy.get('input:invalid').should('have.length', 1)
     });
   });
 });
