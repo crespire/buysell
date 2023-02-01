f2d0c5e8cd9aac4d2607f6f21adb1553b2e51343
crespire <hello@crespire.dev>
2023-02-01 17:03:16 -0500

    feat: add admin user fixture

--- client/cypress/e2e/post_index.cy.ts
+++ client/cypress/e2e/post_index.cy.ts
@@ -6,6 +6,6 @@ describe('Post Index', () => {
     cy.intercept('GET', '/posts', { statusCode: 200, fixture: 'posts_index.json' });
     cy.get('[data-testid="post-index"]').contains('Test Post 1 Body');
     cy.get('[data-testid="post-index"]').contains('Test Post 2 Body');
-    cy.get('[data-testid="post-index"]').children().should('have.length', 2);
+    cy.get('[data-testid="post-index"]').children().should('have.length', 4);
   });
 });
