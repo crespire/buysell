bab2995e6d249262d3b949338b694c4a3e8d0c25
crespire <hello@crespire.dev>
2023-02-16 18:02:43 -0500

    feat: start Post styling
    * Add dropdown menu for post actions
    * Update styles

--- client/src/components/Post.tsx
+++ client/src/components/Post.tsx
@@ -5,6 +5,8 @@ import { useContext } from "react";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { deletePost } from "../providers/PostsApi";
 import { useNavigate } from "react-router";
+import DropdownMenu from "./Dropdown";
+import ImageViewer from "awesome-image-viewer";
 
 function Post(props: PostProps) {
   const { post } = props;
@@ -19,23 +21,33 @@ function Post(props: PostProps) {
       navigate('/')
     }
   });
+  const jsDate = new Date(Date.parse(post?.created_at));
 
   return (
     <article>
       <header>{ post?.title }</header>
-      <span>By { post?.account?.name } posted on { post?.created_at }</span>
-      { user && (user.id === post?.account?.id || user.admin) && 
-        <>
-          <button onClick={() => navigate(`/posts/${post?.id}/edit`)}>Edit</button>
-          <button onClick={() => deletePostMutation.mutate(post?.id)}>Delete</button>
-        </>
-      }
+      <section className='post-info flex'>
+        By { post?.account?.name } { jsDate && `posted on ${jsDate.toLocaleDateString()}.` }
+        { user && (user.id === post?.account?.id || user.admin) && 
+          <DropdownMenu
+            className='post-tools px-1'
+            links={[
+            { name: 'Edit', url: `/posts/${post?.id}/edit` },
+            { name: 'Delete', action: () => deletePostMutation.mutate(post?.id) }
+            ]}
+          >
+            &#10247;
+          </DropdownMenu>
+        }
+      </section>
+      <section className='post-body'>
       <p>{ post?.body }</p>
       { Object.keys(post?.images).length > 0 && (
         <ul>
           { Object.entries(post?.images).map(([name, path]) => <li key={name}><img src={`${baseUrl}${path}`} alt={name} /></li> ) }
         </ul>
       )}
+      </section>
     </article>
   );
 }
