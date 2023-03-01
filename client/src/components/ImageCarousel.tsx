b92f0ed404a0584c31e8ba459c7d4c1a6ccb70b7
crespire <hello@crespire.dev>
2023-03-01 17:39:47 -0500

    wip: start styling image carousel

--- client/src/components/ImageCarousel.tsx
+++ client/src/components/ImageCarousel.tsx
@@ -16,11 +16,13 @@ function ImageCarousel({ images }: ImageCarouselProps) {
   };
 
   return (
-    <div>
-      <img src={images[currentIndex].mainUrl} alt={images[currentIndex].description} />
-      <button onClick={handlePrevClick}>&#129128;</button>
-      <span>{ currentIndex + 1 } of { images.length }</span>
-      <button onClick={handleNextClick}>&#129130;</button>
+    <div className="p-2 bg-neutral-content dark:bg-base-300 flex flex-col justify-center content-center gap-3">
+      <img className="max-w-fit max-h-fit" src={images[currentIndex].mainUrl} alt={images[currentIndex].description} />
+      <section className="flex justify-center content-center gap-x-3">
+        <button onClick={handlePrevClick}>&#129128;</button>
+        <span>{ currentIndex + 1 } of { images.length }</span>
+        <button onClick={handleNextClick}>&#129130;</button>
+      </section>
     </div>
   );
 };
