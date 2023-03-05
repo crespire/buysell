18dbc956fc2714e5a117acdbf394ead862027460
crespire <hello@crespire.dev>
2023-03-04 21:03:11 -0500

    fix: add deploy CORS config

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -7,10 +7,14 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001', '127.0.0.1:3001', 'http://localhost:3000', '127.0.0.1:3000'
+    # origins 'http://localhost:3001', '127.0.0.1:3001', 'http://localhost:3000', '127.0.0.1:3000'
+
+    origins 'buysell.crespire.dev', 'buysell-backend.crespire.dev'
     resource '*', headers: :any, methods: %i[get post put patch delete options head], credentials: true
   end
 end
 
-Rails.application.config.hosts << 'localhost:3001'
-Rails.application.config.hosts << 'localhost:3000'
+# Rails.application.config.hosts << 'localhost:3001'
+# Rails.application.config.hosts << 'localhost:3000'
+Rails.application.config.hosts << 'buysell.crespire.dev'
+Rails.application.config.hosts << 'buysell-backend.crespire.dev'
