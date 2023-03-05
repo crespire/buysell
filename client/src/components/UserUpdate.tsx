adb84dd37fb137f37fec5577acb7d8827b7fc02b
crespire <hello@crespire.dev>
2023-03-04 22:10:20 -0500

    fix: style user update form

--- client/src/components/UserUpdate.tsx
+++ client/src/components/UserUpdate.tsx
@@ -15,36 +15,46 @@ function UserUpdate() {
   }, []);
 
   return (
-    <div>
-      <h1 className="text-2xl">Update Profile</h1>
-      { authErrors && <div className="authinfo text-red-800">Something went wrong, check the requirements and try again.</div> }
-      { authSuccess && <div className="authinfo text-green-800">Changes were successful!</div> }
-      <form className="flex flex-col align-center justify-center space-x-2" onSubmit={(e) => {handleSubmit(e, values['password'], values['new-password'], values['name'])}}>
-        <ul className="list-inside list-disc">
+    <div className="flex flex-col p-4 gap-4 w-full max-w-screen-sm">
+      <h1 className="text-5xl">Update Profile</h1>
+      { authErrors && <div className="authinfo alert alert-error">Something went wrong, check the requirements and try again.</div> }
+      { authSuccess && <div className="authinfo alert alert-success">Changes were successful!</div> }
+      <form className="flex flex-col align-center justify-center gap-2" onSubmit={(e) => {handleSubmit(e, values['password'], values['new-password'], values['name'])}}>
+        <ul className="bg-info p-2 list-inside list-disc">
           <li>Current password must be provided to update information.</li>
           <li>Password must be 8 characters long and contain at least one digit.</li>
         </ul>
-        <div className="p-2">
-          <label htmlFor="email">Password*:</label>
-          <input className="border-b border-black border-solid" required type="password" name="password" onChange={handleChange} onBlur={handleBlur} value={values['password'] || ''} pattern="^(?=.*\d).{8,}$|^$" data-error="Minimum length is 8, and must contain a digit." />
-          { errors.password && <p className="error text-red-500">{errors.password}</p> }
+        <div className="form-control">
+          <label htmlFor="email" className="input-group">
+            <span className="w-1/3">Password<sup>*</sup></span>
+            <input className="input input-bordered w-full" required type="password" name="password" onChange={handleChange} onBlur={handleBlur} value={values['password'] || ''} pattern="^(?=.*\d).{8,}$|^$" data-error="Minimum length is 8, and must contain a digit." />
+          </label>
+          { errors.password && <p className="error alert alert-error">{errors.password}</p> }
         </div>
-        <div className="p-2">
-          <label htmlFor="pass">New Password: </label>
-          <input className="border-b border-black border-solid" type="password" name="new-password" onChange={handleChange} onBlur={handleBlur} value={values['new-password'] || ''} pattern="^(?=.*\d).{8,}$|^$" data-error="Minimum length is 8, and must contain a digit." />
-          { errors['new-password'] && <p className="error text-red-500">{errors['new-password']}</p> }
+        <div className="form-control">
+          <label htmlFor="pass" className="input-group">
+            <span className="w-1/3">New Password</span>
+            <input className="input input-bordered w-full" type="password" name="new-password" onChange={handleChange} onBlur={handleBlur} value={values['new-password'] || ''} pattern="^(?=.*\d).{8,}$|^$" data-error="Minimum length is 8, and must contain a digit." />
+          </label>
+          { errors['new-password'] && <p className="error alert alert-error">{errors['new-password']}</p> }
         </div>
-        <div className="p-2">
-          <label htmlFor="passconf">Confirm: </label>
-          <input className="border-b border-black border-solid" type="password" name="passconf" onChange={handleChange} onBlur={handleBlur} value={values['passconf'] || ''} pattern={`^${values['new-password']}$`} data-error="Passwords must match." />
-          { errors.passconf && <p className="error text-red-500">{errors.passconf}</p> }
+        <div className="form-control">
+          <label htmlFor="passconf" className="input-group">
+            <span className="w-1/3">Confirm</span>
+            <input className="input input-bordered w-full" type="password" name="passconf" onChange={handleChange} onBlur={handleBlur} value={values['passconf'] || ''} pattern={`^${values['new-password']}$`} data-error="Passwords must match." />
+          </label>
+          { errors.passconf && <p className="error alert alert-error">{errors.passconf}</p> }
         </div>
-        <div className="p-2">
-          <label htmlFor="name">Username: </label>
-          <input className="border-b border-black border-solid" type="text" name="name" onChange={handleChange} onBlur={handleBlur} value={values['name'] || ''} pattern="^\w{3,36}$|^$" data-error="Must be between 3 and 36 characters, and only contain word characters." />
-          { errors.name && <p className="error text-red-500">{errors.name}</p> }
+        <div className="form-control">
+          <label htmlFor="name" className="input-group">
+            <span className="w-1/3">Username</span>
+            <input className="input input-bordered w-full" type="text" name="name" onChange={handleChange} onBlur={handleBlur} value={values['name'] || ''} pattern="^\w{3,36}$|^$" data-error="Must be between 3 and 36 characters, and only contain word characters." />
+          </label>
+          { errors.name && <p className="error alert alert-error">{errors.name}</p> }
         </div>
-        <button className="p-2 border border-solid border-black" type="submit">Submit</button>
+        <div className="form-control">
+          <button className="btn" type="submit">Submit</button>
+        </div>
       </form>
     </div>
   );
