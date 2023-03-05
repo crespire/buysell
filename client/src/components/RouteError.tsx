f4d683d279b74d283008a0412ef1c6715218cbf7
crespire <hello@crespire.dev>
2023-03-04 22:27:26 -0500

    fix: update 404 page

--- client/src/components/RouteError.tsx
+++ client/src/components/RouteError.tsx
@@ -7,7 +7,7 @@ function RouteError(): JSX.Element {
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content text-center">
         <div className="max-w-md">
-          <h1 className="text-5xl font-bold">Oops! - 404</h1>
+          <h1 className="text-5xl font-bold">Oops! (404)</h1>
           <p className="py-6">Looks like this page doesn't exist.</p>
           <button className="btn btn-primary" onClick={() => { navigate('/') }}>Go home!</button>
         </div>
