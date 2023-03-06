bd860ec2ad1714b7856b418b704e957d5a8e9121
crespire <hello@crespire.dev>
2023-03-06 09:04:11 -0500

    fix: update max image width

--- client/src/components/ImageCarousel.tsx
+++ client/src/components/ImageCarousel.tsx
@@ -17,7 +17,7 @@ function ImageCarousel({ images }: ImageCarouselProps) {
 
   return (
     <div className="p-2 bg-neutral-content dark:bg-base-300 flex flex-col justify-center content-center gap-3 w-fit justify-self-center self-center">
-      <img className="mx-auto w-fit h-fit max-w-full max-h-full" src={images[currentIndex].mainUrl} alt={images[currentIndex].description} />
+      <img className="mx-auto w-fit h-fit max-w-screen-sm max-h-full" src={images[currentIndex].mainUrl} alt={images[currentIndex].description} />
       { images.length > 1 && (
         <section className="flex justify-center content-center gap-x-3">
           <button onClick={handlePrevClick}>&#129128;</button>
